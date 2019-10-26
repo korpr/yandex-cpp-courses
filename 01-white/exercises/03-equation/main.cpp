e0e0e63d3bb020f33509c4b865aa012e9f7baeca
Sergey Gordev <sergey.gordeev@jrholding.co>
2019-10-26 20:38:48 +0300

    remove cerr

--- 01-white/exercises/03-equation/main.cpp
+++ 01-white/exercises/03-equation/main.cpp
@@ -3,30 +3,29 @@
 
 int main(int argc, char **argv)
 {
-	double a{0};
-	double b{0};
-	double c{0};
-	std::cin >> a >> b >> c;
-	if (a == 0) {
-		if (b != 0) {
-			std::cout << -c / b;
-		}
-		return 0;
-	}
-	double d = std::pow(b,2) - 4*a*c;
-	if (d < 0) {
-		//no solution
-		return 0;
-	}
+    double a{0};
+    double b{0};
+    double c{0};
+    std::cin >> a >> b >> c;
+    if (a == 0) {
+        if (b != 0) {
+            std::cout << -c / b;
+        }
+        return 0;
+    }
+    double d = std::pow(b,2) - 4*a*c;
+    if (d < 0) {
+        //no solution
+        return 0;
+    }
 
-	double sqrtD {std::sqrt(d)};
-	std::cerr << sqrtD << std::endl;
-	double v1 = -(b + sqrtD)/(2*a);
-	double v2 = -(b - sqrtD)/(2*a);
+    double sqrtD {std::sqrt(d)};
+    double v1 = -(b + sqrtD)/(2*a);
+    double v2 = -(b - sqrtD)/(2*a);
 
-	std::cout << v1;
-	if (v1 != v2) {
-		std::cout << " " << v2;
-	}
-	return 0;
+    std::cout << v1;
+    if (v1 != v2) {
+        std::cout << " " << v2;
+    }
+    return 0;
 }
