04fbc7d204a0f89d03f34147acaa11808ebac3d3
Sergey Gordev <sergey.gordeev@jrholding.co>
2019-10-29 11:04:07 +0300

    Solve 11 polidrome

---
+++ 01-white/exercises/11-palindrome/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+bool IsPalindrom ( std::string& text )
+{
+    size_t lastElem = text.size() - 1;
+    size_t center = text.size() / 2 ;
+    std::cerr << center;
+    for ( size_t idx = 0; idx <  center ; ++idx ) {
+        if ( text[idx] != text[lastElem - idx] ) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main ( int argc, char **argv )
+{
+    std::string a;
+    std::cin >> a;
+    std::cout << std::boolalpha << IsPalindrom ( a );
+    return 0;
+}
