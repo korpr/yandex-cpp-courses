6bf1b62f09cc5dfb421fb93d7e9099b63bdd4b19
Sergey Gordev <sergey.gordeev@jrholding.co>
2019-11-04 12:08:32 +0300

    Refactoring for 18- queu

--- 01-white/exercises/18-queue/main.cpp
+++ 01-white/exercises/18-queue/main.cpp
@@ -13,27 +13,19 @@ int main ( int argc, char **argv )
     for ( int i = 0; i < n; i++ ) {
         cin >> command;
         if ( command == "WORRY_COUNT" ) {
-            cout << count ( people.begin(), people.end(), true );
-            continue;
-        }
+            cout << count ( people.begin(), people.end(), true ) << endl;
+        }else
         if ( command == "COME" ) {
             int delta = 0;
             cin >> delta;
             //todo:check if new  sizev less then zero
-            people.resize ( people.size() + delta );
+            people.resize ( people.size() + delta, false );
             continue;
-        }
-        if ( command == "QUIET" ) {
+        }else
+        if ( command == "QUIET" || command == "WORRY" ) {
             int idx = 0;
             cin >> idx;
-            people[idx] = false;
-            continue;
-        }
-        
-        if ( command == "WORRY" ) {
-            int idx = 0;
-            cin >> idx;
-            people[idx] = true;
+            people[idx] = command == "WORRY";
             continue;
         }
     }
