40a34640214db3bece9627fdc2919b0cbbacaab1
Sergey Gordev <sergey.gordeev@jrholding.co>
2020-01-17 13:00:17 +0300

    solution for 14-split-words

---
+++ 02-yellow/excersises/w03-14-split-worlds/w03-15-split-worlds/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  w03-14-split-worlds
+//
+//  Created by Sergey Gordeev on 17/01/2020.
+//  Copyright © 2020 Sergey Gordeev. All rights reserved.
+//
+
+#include <iostream>
+#include "test_runner.hpp"
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
