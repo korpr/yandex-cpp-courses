af2a56f48cbb0f302beef7f8981d55bdd483b715
Sergey Gordev <sergey.gordeev@jrholding.co>
2020-01-19 16:23:28 +0300

    solution for w04-18-demographic-stats

---
+++ 02-yellow/excersises/w04-18-demographic-stats/w04-18-demographic-stats/ststicstics.hpp
@@ -0,0 +1,14 @@
+//
+//  ststicstics.hpp
+//  w04-18-demographic-stats
+//
+//  Created by Sergey Gordeev on 19/01/2020.
+//  Copyright © 2020 Sergey Gordeev. All rights reserved.
+//
+
+#ifndef ststicstics_hpp
+#define ststicstics_hpp
+
+#include <stdio.h>
+
+#endif /* ststicstics_hpp */
