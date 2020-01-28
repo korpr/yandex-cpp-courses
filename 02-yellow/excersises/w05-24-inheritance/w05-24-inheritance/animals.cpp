bb215c6586592445e73b1394ad9cc7ae507ae95f
Sergey Gordev <sergey.gordeev@jrholding.co>
2020-01-28 20:54:28 +0300

    solution for w05-24-inheritance

---
+++ 02-yellow/excersises/w05-24-inheritance/w05-24-inheritance/animals.cpp
@@ -0,0 +1,9 @@
+//
+//  animals.cpp
+//  w05-24-inheritance
+//
+//  Created by Sergey Gordeev on 28.01.2020.
+//  Copyright © 2020 Sergey Gordeev. All rights reserved.
+//
+
+#include "animals.hpp"
